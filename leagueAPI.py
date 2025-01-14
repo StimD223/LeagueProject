a77d87c852ff3072509c0c73d6c275a57852635b
Quinn <panosqui10@gmail.com>
2025-01-13 20:47:12 -0600

    database start plus profile icons

--- leagueAPI.py
+++ leagueAPI.py
@@ -1,10 +1,13 @@
 import requests
 from flask import Flask, render_template, request
 import re
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///profiles.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':

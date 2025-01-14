c9781ddc85ab25391f9797a7c28fd4a79dc8ffe8
Quinn <panosqui10@gmail.com>
2025-01-13 21:07:57 -0600

    restarting

--- leagueAPI.py
+++ leagueAPI.py
@@ -5,9 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///profiles.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
